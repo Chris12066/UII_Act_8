d4c27721698586031fa3402dd2db469d3d62bb48
CHRISTIAN_ALEXIS_ALCANTARA_GONZALEZ <a.21308051280308@cbtis128.edu.mx>
2024-04-26 17:53:21 +0000

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,93 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const AppMiTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppMiTabBar extends StatelessWidget {
+  const AppMiTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo Tabbar Christian Alcantara",
+      theme: ThemeData(primarySwatch: Colors.lime),
+      home: const MiPaginaInicial(),
     );
   }
-}
+} // fin AppMiTabBar
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+//stateful
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+}
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black26,
+          title: const Text("Concesionaria y refaccionaria",
+              style: TextStyle(color: Colors.white)),
+          centerTitle: true,
+          bottom: const TabBar(tabs: [
+            Tab(
+              text: "180SX",
+              icon: Icon(Icons.car_rental),
+            ),
+            Tab(
+              text: "S15",
+              icon: Icon(Icons.car_rental),
+            ),
+            Tab(
+              text: "NSX",
+              icon: Icon(Icons.car_rental),
+            ),
+            Tab(
+              text: "S14",
+              icon: Icon(Icons.car_rental),
+            ),
+          ] //fin de tab
+              ), //fin bottom tabbar
+        ),
+        body: TabBarView(children: <Widget>[
+          _buildImage(
+              "https://raw.githubusercontent.com/Chris12066/Img_iOS/main/FlutterFlowA13/180sx.jpg",
+              Colors.blue), // Estilo diferente para la primera imagen
+          _buildImage(
+              "https://raw.githubusercontent.com/Chris12066/Img_iOS/main/FlutterFlowA13/s15.jpg",
+              Colors.green), // Estilo diferente para la segunda imagen
+          _buildImage(
+              "https://raw.githubusercontent.com/Chris12066/Img_iOS/main/FlutterFlowA13/nsx.jpg",
+              Colors.red), // Estilo diferente para la tercera imagen
+          _buildImage(
+              "https://raw.githubusercontent.com/Chris12066/Img_iOS/main/FlutterFlowA13/s14.jpg",
+              Colors.orange), // Estilo diferente para la cuarta imagen
+        ] //jardin de niños
+            ),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    ); //DefaultTabController
+  } //fin widgets
+
+  Widget _buildImage(String imageUrl, Color color) {
+    return Center(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(60.0),
+        child: Container(
+          color: color, // Cambia el color de fondo de la imagen
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+            width: 300,
+            height: 300,
+          ),
         ),
       ),
     );
   }
-}
+} // _MiPaginaInicialState
